369941385133b5a4d61cdf2a441cb2a64767ee9a
daniel <lesamessipa@gmail.com>
2023-02-22 02:00:58 +0100

    initaion off the project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+    <header>
+        <div class="wrapper">
+            <nav>
+                <RouterLink to="/">Acceuil</RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
